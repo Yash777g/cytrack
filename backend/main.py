a231f9ca75e98d823a034594405488cdfc644fb7
sejal-collab <sejals2005@gmail.com>
2026-09-09 09:35:13 +0530

    removed backend folder from repo

--- backend/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from database import engine, Base
-
-app = FastAPI()
-Base.metadata.create_all(bind=engine)
-
-@app.get("/")
-def check():
-    return {"message": "Database connected, tables created!"}
